394995ec6add93f3d80e345a548a920b84e9a3b2
agneeval <agneseevald@gmail.com>
2022-05-31 23:22:06 +0300

    YourName

---
+++ YourName.h
@@ -0,0 +1,14 @@
+#include <iosteam>
+#include "Name.h"
+using namespace std;
+
+class YourName: public Name
+{
+    public:
+            YourName(string = "DefaultName");
+            virtual void setName(string);
+            void Print() const;
+
+            private:
+                    string name;
+};
