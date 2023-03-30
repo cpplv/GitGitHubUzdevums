807e84741f2432ec259cefd1719dfe3a89879130
laimesberns <apoimanovs@gmail.com>
2023-03-30 19:50:34 +0100

    Pievienots Artura vards

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include "Name.h"
 #include "Andrejs.h"
+#include "Arturs.h"
 using namespace std;
 
 int main()
 {
-	const int array_size = 1;
+	const int array_size = 2;
 	Name *Names[array_size];
 	
 	Andrejs a;
 	a.setName("Andrejs");
 	
+	Arturs aa;
+	aa.setName("Archi");
+	
 	Names[0] = &a;
+	Names[1] = &aa;
 	
 	for (int i=0; i<array_size; i++)
 		Names[i]->Print();
