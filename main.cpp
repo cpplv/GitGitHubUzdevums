1cc8f893d1fc28599d036a2cd4df8b972c9bb215
Lauris Broks <Broks.lauris@gmail.com>
2023-06-10 15:03:48 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@ using namespace std;
 
 int main()
 {
-	const int array_size = 1;
+	const int array_size = 2;
 	Name *Names[array_size];
 	
 	Andrejs a;
 	a.setName("Andrejs");
 	
+	Lauris b;
+	b.setName("Lauris");
+
+
 	Names[0] = &a;
+	Names[0] = &b;
 	
 	for (int i=0; i<array_size; i++)
 		Names[i]->Print();
