b8b1dc508d854b37a9caa3797ffe2ac9dc63849e
Kristaps Kukurans <kristaps.kukurans@gmail.com>
2024-03-14 00:34:11 +0200

    Pievienoti Kristaps.cpp un Kristaps.h un veiktas izmainas main.cpp failaa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Name.h"
 #include "Andrejs.h"
+#include "Kristaps.h"
 using namespace std;
 
 int main()
@@ -10,8 +11,12 @@ int main()
 	
 	Andrejs a;
 	a.setName("Andrejs");
+		
+	Kristaps a;
+	a.setName("Kristaps");
 	
 	Names[0] = &a;
+	Names[1] = &k;
 	
 	for (int i=0; i<array_size; i++)
 		Names[i]->Print();
